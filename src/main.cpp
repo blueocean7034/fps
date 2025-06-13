c7c30dbe9f156b78527721375676556459c26d87
blueocean7034 <blueocean7034@gmail.com>
2025-06-12 19:42:02 -0500

    Add random no_texture default surfaces

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,9 @@
 #include <iostream>
 #include <vector>
 #include <cmath>
+#include <filesystem>
+#include <random>
+#include "stb_image.h"
 
 struct Camera {
     glm::vec3 position{0.0f, 1.0f, 0.0f};
@@ -54,6 +57,39 @@ GLuint createProgram(const char* vsSrc, const char* fsSrc) {
     glDeleteShader(vs);
     glDeleteShader(fs);
     return prog;
+}
+
+GLuint loadTexture(const std::string& path) {
+    int w, h, channels;
+    stbi_uc* data = stbi_load(path.c_str(), &w, &h, &channels, STBI_rgb_alpha);
+    if (!data) {
+        std::cerr << "Failed to load " << path << std::endl;
+        return 0;
+    }
+    GLuint tex;
+    glGenTextures(1, &tex);
+    glBindTexture(GL_TEXTURE_2D, tex);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, w, h, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+    glGenerateMipmap(GL_TEXTURE_2D);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST_MIPMAP_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    stbi_image_free(data);
+    return tex;
+}
+
+std::vector<GLuint> loadNoTextureVariants() {
+    namespace fs = std::filesystem;
+    std::vector<GLuint> textures;
+    for (auto& p : fs::directory_iterator("images")) {
+        std::string fname = p.path().filename().string();
+        if (fname.rfind("no_texture", 0) == 0 && p.path().extension() == ".png") {
+            GLuint tex = loadTexture(p.path().string());
+            if (tex) textures.push_back(tex);
+        }
+    }
+    return textures;
 }
 
 bool initSDL(SDL_Window** window, SDL_GLContext* context, int width, int height) {
@@ -143,42 +179,81 @@ int main(int argc, char** argv) {
         "#version 330 core\n"
         "layout(location = 0) in vec3 aPos;\n"
         "layout(location = 1) in vec3 aColor;\n"
+        "layout(location = 2) in vec2 aTex;\n"
         "out vec3 vColor;\n"
+        "out vec2 vTex;\n"
         "uniform mat4 uMVP;\n"
         "void main() {\n"
         "    vColor = aColor;\n"
+        "    vTex = aTex;\n"
         "    gl_Position = uMVP * vec4(aPos, 1.0);\n"
         "}";
 
     const char* fsSrc =
         "#version 330 core\n"
         "in vec3 vColor;\n"
+        "in vec2 vTex;\n"
         "out vec4 FragColor;\n"
+        "uniform sampler2D uTex;\n"
         "void main() {\n"
-        "    FragColor = vec4(vColor, 1.0);\n"
+        "    FragColor = texture(uTex, vTex) * vec4(vColor, 1.0);\n"
         "}";
 
     GLuint program = createProgram(vsSrc, fsSrc);
+    glUseProgram(program);
+    glUniform1i(glGetUniformLocation(program, "uTex"), 0);
+
+    std::vector<GLuint> noTextures = loadNoTextureVariants();
+    if (noTextures.empty()) {
+        std::cerr << "No placeholder textures found" << std::endl;
+        return -1;
+    }
+
+    std::mt19937 rng(SDL_GetTicks());
+    std::uniform_int_distribution<size_t> dist(0, noTextures.size() - 1);
+    GLuint faceTex[6];
+    for (int i = 0; i < 6; ++i) faceTex[i] = noTextures[dist(rng)];
 
     float vertices[] = {
-        // positions            // colors
-        -10.0f, 0.0f, -10.0f,   0.7f, 0.7f, 0.7f,
-         10.0f, 0.0f, -10.0f,   0.7f, 0.7f, 0.7f,
-         10.0f, 5.0f, -10.0f,   0.7f, 0.7f, 0.7f,
-        -10.0f, 5.0f, -10.0f,   0.7f, 0.7f, 0.7f,
-        -10.0f, 0.0f,  10.0f,   0.7f, 0.7f, 0.7f,
-         10.0f, 0.0f,  10.0f,   0.7f, 0.7f, 0.7f,
-         10.0f, 5.0f,  10.0f,   0.7f, 0.7f, 0.7f,
-        -10.0f, 5.0f,  10.0f,   0.7f, 0.7f, 0.7f
+        // pos                 // color          // tex
+        -10.f,0.f,-10.f, 0.7f,0.7f,0.7f, 0.f,0.f,
+         10.f,0.f,-10.f, 0.7f,0.7f,0.7f, 1.f,0.f,
+         10.f,5.f,-10.f, 0.7f,0.7f,0.7f, 1.f,1.f,
+        -10.f,5.f,-10.f, 0.7f,0.7f,0.7f, 0.f,1.f,
+
+        -10.f,0.f,10.f, 0.7f,0.7f,0.7f, 0.f,0.f,
+         10.f,0.f,10.f, 0.7f,0.7f,0.7f, 1.f,0.f,
+         10.f,5.f,10.f, 0.7f,0.7f,0.7f, 1.f,1.f,
+        -10.f,5.f,10.f, 0.7f,0.7f,0.7f, 0.f,1.f,
+
+        -10.f,0.f,-10.f, 0.7f,0.7f,0.7f, 0.f,0.f,
+        -10.f,0.f,10.f, 0.7f,0.7f,0.7f, 1.f,0.f,
+        -10.f,5.f,10.f, 0.7f,0.7f,0.7f, 1.f,1.f,
+        -10.f,5.f,-10.f,0.7f,0.7f,0.7f, 0.f,1.f,
+
+         10.f,0.f,-10.f,0.7f,0.7f,0.7f, 0.f,0.f,
+         10.f,0.f,10.f, 0.7f,0.7f,0.7f, 1.f,0.f,
+         10.f,5.f,10.f, 0.7f,0.7f,0.7f, 1.f,1.f,
+         10.f,5.f,-10.f,0.7f,0.7f,0.7f, 0.f,1.f,
+
+        -10.f,5.f,-10.f,0.7f,0.7f,0.7f, 0.f,0.f,
+         10.f,5.f,-10.f,0.7f,0.7f,0.7f, 1.f,0.f,
+         10.f,5.f,10.f, 0.7f,0.7f,0.7f, 1.f,1.f,
+        -10.f,5.f,10.f, 0.7f,0.7f,0.7f, 0.f,1.f,
+
+        -10.f,0.f,-10.f,0.7f,0.7f,0.7f, 0.f,0.f,
+         10.f,0.f,-10.f,0.7f,0.7f,0.7f, 1.f,0.f,
+         10.f,0.f,10.f, 0.7f,0.7f,0.7f, 1.f,1.f,
+        -10.f,0.f,10.f, 0.7f,0.7f,0.7f, 0.f,1.f
     };
 
     unsigned int indices[] = {
         0,1,2, 2,3,0,
         4,5,6, 6,7,4,
-        4,7,3, 3,0,4,
-        1,5,6, 6,2,1,
-        3,2,6, 6,7,3,
-        4,5,1, 1,0,4
+        8,9,10, 10,11,8,
+        12,13,14, 14,15,12,
+        16,17,18, 18,19,16,
+        20,21,22, 22,23,20
     };
 
     GLuint VAO, VBO, EBO;
@@ -191,10 +266,12 @@ int main(int argc, char** argv) {
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
     glEnableVertexAttribArray(1);
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+    glEnableVertexAttribArray(2);
     glBindVertexArray(0);
 
 
@@ -229,7 +306,10 @@ int main(int argc, char** argv) {
         glUseProgram(program);
         glUniformMatrix4fv(glGetUniformLocation(program, "uMVP"), 1, GL_FALSE, glm::value_ptr(mvp));
         glBindVertexArray(VAO);
-        glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, 0);
+        for (int i = 0; i < 6; ++i) {
+            glBindTexture(GL_TEXTURE_2D, faceTex[i]);
+            glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, (void*)(i * 6 * sizeof(unsigned int)));
+        }
         glBindVertexArray(0);
         SDL_GL_SwapWindow(window);
     }
@@ -243,3 +323,6 @@ int main(int argc, char** argv) {
     SDL_Quit();
     return 0;
 }
+
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
